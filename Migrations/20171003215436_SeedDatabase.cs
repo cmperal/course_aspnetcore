b43d76b568e497334a2ca7d40d16a6a23d31406d
Carlos Martin Peral <cmperal>
2017-10-04 00:42:45 +0200

    Migrations added and seeding database.

---
+++ Migrations/20171003215436_SeedDatabase.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace vega.Migrations
+{
+    public partial class SeedDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Makes (Name) Values ('Make1')");
+            migrationBuilder.Sql("INSERT INTO Makes (Name) Values ('Make2')");
+            migrationBuilder.Sql("INSERT INTO Makes (Name) Values ('Make3')");
+
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) Values ('Make1-ModelA', (SELECT Id FROM Makes WHERE Name LIKE 'Make1'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) Values ('Make1-ModelB', (SELECT Id FROM Makes WHERE Name LIKE 'Make1'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) Values ('Make1-ModelC', (SELECT Id FROM Makes WHERE Name LIKE 'Make1'))");
+
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) Values ('Make2-ModelA', (SELECT Id FROM Makes WHERE Name LIKE 'Make2'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) Values ('Make2-ModelB', (SELECT Id FROM Makes WHERE Name LIKE 'Make2'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) Values ('Make2-ModelC', (SELECT Id FROM Makes WHERE Name LIKE 'Make2'))");
+
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) Values ('Make3-ModelA', (SELECT Id FROM Makes WHERE Name LIKE 'Make3'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) Values ('Make3-ModelB', (SELECT Id FROM Makes WHERE Name LIKE 'Make3'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) Values ('Make3-ModelC', (SELECT Id FROM Makes WHERE Name LIKE 'Make3'))");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Makes WHERE Name IN ('Make1', 'Make2', Make3'");
+        }
+    }
+}
--- Models/Make.cs
+++ Models/Make.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace vega.Models
 {
@@ -7,7 +8,9 @@ namespace vega.Models
     {
         public int Id {get; set;}
 
-        public string Name {get; set;}
+        [Required]
+        [StringLength(255)]
+         public string Name {get; set;}
 
         public ICollection<Model> Models {get; set;}    
 
