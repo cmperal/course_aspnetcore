84d14b097092493b6706f517d04e74c7fe4b7d9f
Carlos Martin Peral <cmperal>
2017-10-11 00:31:12 +0200

    Angular Cli has been added

--- ClientApp/app/app.module.shared.ts
+++ ClientApp/app/app.module.shared.ts
@@ -1,3 +1,4 @@
+import { VehiculeFormComponent } from './components/vehicule-form/vehicule-form.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -10,13 +11,14 @@ import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
 
+
 @NgModule({
     declarations: [
         AppComponent,
         NavMenuComponent,
         CounterComponent,
         FetchDataComponent,
-        HomeComponent
+        VehiculeFormComponent
     ],
     imports: [
         CommonModule,
